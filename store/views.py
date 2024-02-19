19c49db36e560442afd6a0cd67425f269a52e378
Gesare5 <gesare.nyangweso@gmail.com>
2024-02-19 14:52:39 +0300

    Add:  Put and Delete methods for Store Detail View

--- store/views.py
+++ store/views.py
@@ -58,31 +58,19 @@ class StoreDetailView(APIView):
         serializer = StoreSerializer(store)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-        #   def get_object(self, pk):
-        # try:
-        # return Product.objects.get(pk=pk)
-        # except Product.DoesNotExist:
-        # raise Http404
+    def put(self, request, pk, format=None):
+        store = self.get_object(pk)
+        serializer = StoreSerializer(store, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk, format=None):
+        store = self.get_object(pk)
+        store.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-# class BrandDetailView(APIView):
-#     """
-#     Update Brand details
-#     Retrieve Brand,
-#     Change detail,
-#     Save changes to db
-#     """
-
-#     def get_object(self, pk):
-#         try:
-#             return Brand.objects.get(pk=pk)
-#         except Brand.DoesNotExist:
-#             raise Http404
-
-#     def get(self, pk, format=None):
-#         brand = self.get_object(pk)
-#         serializer = BrandSerializer(brand)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
 
 #     def put(self, request, pk, format=None):
 #         brand = self.get_object(pk)
