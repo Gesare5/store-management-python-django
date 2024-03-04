ba1d9295e7e9a68dfac4a24e1d8e60821678b55f
Gesare5 <gesare.nyangweso@gmail.com>
2024-03-04 15:05:39 +0300

    Add: Auth user model variable to settings.py Change: Update user model Change: Include UserManager class

--- user/models.py
+++ user/models.py
@@ -1,23 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.utils import timezone
 
-from store.models import Store
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        now = timezone.now()
+        if not email:
+            raise ValueError(('The email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, is_staff=is_staff, 
+                          is_active=True, is_superuser=is_superuser,
+                          last_login=now, created_at=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, ' ', **extra_fields)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        user = self._create_user(email, password, True, True, **extra_fields)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
 
 
-class User(models.Model):
-    name = models.CharField(max_length=150)
-    email = models.CharField(max_length=200)
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    store = models.ForeignKey(Store, null=True, on_delete=models.SET_NULL)
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.CharField(max_length=250, unique=True)
+    first_name = models.CharField(max_length=30, Blank=True, null=True)
+    last_name = models.CharField(max_length=30, Blank=True, null=True)
+    phone = models.CharField(max_length=30, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    country = models.CharField(max_length=30, Blank=True, null=True)
+    city = models.CharField(max_length=30, Blank=True, null=True)
 
-    def __str__(self):
-        return self.name
+    objects = UserManager()
 
-    # figure out how to link django's user class/object
-    # setup custom urls
-    # check out custom user creation
-    # figure out how to return selected users store/products etc
-    # products/brands are global
-    # formatting of phone numbers and dates
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email', ]
