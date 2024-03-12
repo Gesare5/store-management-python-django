2c131e4c8d92862723bd900516e86088bc7f02ad
Gesare5 <gesare.nyangweso@gmail.com>
2024-03-12 17:22:09 +0300

    Fix:  Add null property to user field on Store model

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ from user.models import User
 
 
 class Store(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=150)
     number = models.CharField(max_length=16)
     description = models.TextField(blank=True)
